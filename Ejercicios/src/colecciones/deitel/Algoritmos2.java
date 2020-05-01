efa645a7e02c36c6b236f0cbced7d503fffed293
damodar88 <damodar88@icloud.com>
2020-05-01 19:31:51 -0400

    se agrega ejercicio de vector

--- Ejercicios/src/colecciones/deitel/Algoritmos2.java
+++ Ejercicios/src/colecciones/deitel/Algoritmos2.java
@@ -1,5 +1,8 @@
 package colecciones.deitel;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
 
@@ -9,9 +12,42 @@ public class Algoritmos2 {
 	private List< String >  lista;
 	private Vector< String > vector = new Vector< String >();
 	
+	//crea objetos List y vector
+	public Algoritmos2() {
+		
+		//iniciliza lista vector
+		lista = Arrays.asList( colores );
+		vector.add( "negro" );
+		vector.add( "rojo" );
+		vector.add( "verde" );
+		
+		System.out.println("Antes de addAll, el vector contiene: ");
+		
+		//muestra elementos de vector
+		for ( String s : vector) {
+			System.out.printf("%s", s);
+		}
+		
+		//agrega los elementos en colores a list
+		Collections.addAll( vector, colores);
+		
+		System.out.println("\n\nDespues de addAll, el vector contiene: ");
+		
+		for ( String s : vector ) {
+			System.out.printf("%s", s);
+		}
+		
+		int frecuencia = Collections.frequency( vector, "rojo");
+		System.out.printf("\n\nFrecuencia de rojo en el vector: %d\n", frecuencia);
+		
+		boolean desunion = Collections.disjoint( lista, vector );
+		System.out.printf("\nlista y vector %s elementos en comun\n", ( desunion ? "no tienen" : "tienen" ));
+	
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Algoritmos2();
 	}
 
 }
---
+++ Ejercicios/src/colecciones/deitel/PruebaStack.java
@@ -0,0 +1,17 @@
+package colecciones.deitel;
+
+import java.util.Stack;
+
+public class PruebaStack {
+	
+	public PruebaStack() {
+		
+		Stack< Number > pila = new Stack< Number >();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
