4891ddf64b3b384c8c4052aa6b62f94c1cb177b8
damodar88 <damodar88@icloud.com>
2020-05-01 19:00:20 -0400

    prueba ssh

---
+++ Ejercicios/src/colecciones/deitel/Algoritmos2.java
@@ -0,0 +1,17 @@
+package colecciones.deitel;
+
+import java.util.List;
+import java.util.Vector;
+
+public class Algoritmos2 {
+
+	private String[] colores = {"rojo", "blanco", "amarillo", "azul"};	
+	private List< String >  lista;
+	private Vector< String > vector = new Vector< String >();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
