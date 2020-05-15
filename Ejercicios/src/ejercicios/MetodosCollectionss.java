6febb872db1edfed2e043625e708c48a26a9a4ed
damodar88 <damodar88@icloud.com>
2020-05-14 20:21:25 -0400

    se agregan ejercicos de collections

--- Ejercicios/src/ejercicios/MetodosCollectionss.java
+++ Ejercicios/src/ejercicios/MetodosCollectionss.java
@@ -17,18 +17,18 @@ public class MetodosCollectionss {
 	public MetodosCollectionss(){
 		lista = Arrays.asList( colores );
 		
-		vector.add("taique");
-		vector.add("nogal");
-		vector.add("hualle");
-		vector.add("araucaria");
+		vector.add( "taique" );
+		vector.add( "nogal" );
+		vector.add( "hualle" );
+		vector.add( "araucaria" );
 		
-		System.out.println("Lista :");
+		System.out.println( "Lista :" );
 		imprimir( lista );
 		
-		System.out.println("\n\nAntes del All: ");
+		System.out.println( "\n\nAntes del All: " );
 		imprimir( vector );
 		Collections.addAll( vector , colores);
-		System.out.println("\n\nDespues del All: ");
+		System.out.println( "\n\nDespues del All: " );
 		imprimir( vector );
 		
 		int frecuencia = Collections.frequency(vector, "amarillo");
@@ -37,8 +37,27 @@ public class MetodosCollectionss {
 		boolean desunion = Collections.disjoint(lista, vector);
 		System.out.printf("\n\nlista y vector %s elementos en comun\n", (desunion ? "no tienen" : "tienen" )," ", desunion );
 		
-		Collections.copy( lista2 ,  vector );
+		//colores = lista2.toArray( new String[ lista2.size() ] );
+		
+		lista2 = Arrays.asList( colores );
+		
+		Collections.copy( lista2 ,  lista );
 		imprimir( lista2 );
+		
+		System.out.println("\n\nDesordena: ");
+		Collections.shuffle( lista );
+		imprimir(lista);
+		
+		System.out.println("\n\nReverse: ");
+		Collections.reverse( lista );
+		imprimir(lista);
+		
+		System.out.println("\n\nRellena de vere: ");
+		Collections.fill( lista , "verde");
+		imprimir(lista);
+		
+		System.out.println("\n\nOrdenada: ");
+		Collections.sort( lista );
 		
 		
 	}
