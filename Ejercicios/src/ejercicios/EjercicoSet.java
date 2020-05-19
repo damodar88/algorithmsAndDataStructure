0b3b56e519a350ff036dbb9da5503fe5068afc8a
damodar88 <damodar88@icloud.com>
2020-05-19 19:51:20 -0400

    se agrega ejemplo de Set HashSet

--- Ejercicios/src/ejercicios/EjercicoSet.java
+++ Ejercicios/src/ejercicios/EjercicoSet.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
+import java.util.List;/
 import java.util.Set;
 
 public class EjercicoSet {
