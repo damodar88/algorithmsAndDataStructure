c6d34441c9194d94a8a4ba46a3689907850b53d6
damodar88 <damodar88@icloud.com>
2020-05-19 22:56:39 -0400

    se agrega prueba de SortedSet

--- Ejercicios/src/ejercicios/PruebaHash.java
+++ Ejercicios/src/ejercicios/PruebaHash.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
+import java.util.Set;	
 
 public class PruebaHash {
 	
