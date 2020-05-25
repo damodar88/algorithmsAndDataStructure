67955383ded28a89df1c3e231ca89baf5179672f
damodar88 <damodar88@icloud.com>
2020-05-25 11:58:17 -0400

    se agrega ejercicio de recurcividad

---
+++ Ejercicios/src/recursividad/deitel/PruebaFactorial.java
@@ -0,0 +1,10 @@
+//Fig 15.4: Prueba factorial
+//Pag 657 metodo pruebafactorial
+package recursividad.deitel;
+
+public class PruebaFactorial {
+	public static void main(String[] args) {
+		CalculoFactorial calcularFactorial = new CalculoFactorial();	
+		calcularFactorial.mostrarFactorial();
+	}
+}
