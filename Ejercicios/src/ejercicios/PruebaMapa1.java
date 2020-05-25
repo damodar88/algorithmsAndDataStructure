c053e45949fc9c9469a4fccfba350713feaa9032
damodar88 <damodar88@icloud.com>
2020-05-24 21:36:58 -0400

    se agrega ejercicio de recorer mapa

--- Ejercicios/src/ejercicios/PruebaMapa1.java
+++ Ejercicios/src/ejercicios/PruebaMapa1.java
@@ -2,29 +2,50 @@ package ejercicios;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class PruebaMapa1 {
 	
 	private static String nombres[] = { "damodar", "navarrete" };
 	Map< String, Integer > map;
+	private Scanner scanner;
 	
 	public PruebaMapa1() {
+	
 		map = new HashMap< String, Integer >();
 		
-		String cuenta = "damodar";
+		System.out.println("\nIngrese los valores: ");
+		String entrada = scanner.nextLine();
+
+		StringTokenizer  tokenizar = new StringTokenizer( entrada ); 
 		
-		map.put( cuenta , 1);
+		ingresarValoresMapa( tokenizar );
 		
-		for ( String s : nombres ) {
-			int contador = 0;
-			map.put( s , contador + 1 );
-		}
+		imprimirMapa( map );
 		
-		for ( String n : map ) {
+	}
+	private void ingresarValoresMapa( StringTokenizer  tokenizar) {
 			
+		while ( tokenizar.hasMoreTokens() ) {
+			String palabra = tokenizar.nextToken().toLowerCase();
+			if (map.containsKey( palabra ) )// esta la palabra en el mapa
+			{
+				int cuenta = map.get( palabra );
+				map.put( palabra, cuenta + 1 );
+			} else {
+				map.put( palabra, 1 );
+			}
 		}
 		
 	}
+	
+	private void imprimirMapa( Map< String, Integer > map ) {
+		for ( Map.Entry< String, Integer > entry : map.entrySet() ) {
+			System.out.println( "Clave = " + entry.getKey() + "\nValor = " + entry.getValue() );
+		}
+	}
+	
 
 	public static void main(String[] args) {
 		new PruebaMapa1();
