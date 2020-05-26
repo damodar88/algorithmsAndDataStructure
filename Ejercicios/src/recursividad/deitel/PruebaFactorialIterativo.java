24dc083c43dda3cc34eb00178952484f44dae778
damodar88 <damodar88@icloud.com>
2020-05-26 19:27:58 -0400

    se agrega ejercicios de recurcibidad

---
+++ Ejercicios/src/recursividad/deitel/PruebaFactorialIterativo.java
@@ -0,0 +1,10 @@
+package recursividad.deitel;
+
+public class PruebaFactorialIterativo {
+	
+	public static void main(String[] args) {
+		CalculoFactorialIterativo calculoFactorialIterativo = new CalculoFactorialIterativo();
+		calculoFactorialIterativo.mostrarFactoriales();
+	}
+
+}
