804a4aaed19e6ee5427193da282ec397369bce57
damodar88 <damodar88@icloud.com>
2020-05-27 11:52:55 -0400

    se agrega ejercicio de metodo de burbuja

---
+++ Ejercicios/src/ejercicios/Burbuja.java
@@ -0,0 +1,32 @@
+package ejercicios;
+
+public class Burbuja {
+	
+	public static void main(String[] args) {
+		int arreglo[] = { 9,8,7,6,5,4,3,2,1 };
+		int aux;
+		System.out.println("Antes de ordenar: ");
+		
+		for ( int s : arreglo ) {
+			System.out.printf("%s", s);
+		}
+		
+		System.out.printf( "\n" );
+		
+		for ( int i = 0; i < arreglo.length; i++ ) {
+			for ( int j = i + 1; j < arreglo.length; j++ ) {
+				if ( arreglo[i] > arreglo[j] ) {
+					aux = arreglo[i];
+					arreglo[i] = arreglo[j];
+					arreglo[j] = aux;
+				}
+			}
+		}
+		
+		System.out.println( "Arreglo despues de burbuja: " );
+		for ( int i : arreglo ) {
+			System.out.printf( "%s",i );
+		}
+	}
+
+}
